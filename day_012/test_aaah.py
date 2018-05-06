fda75effb7c7ea7dee27a8fcba0453d5b42c6d1d
Ron Sprenkels <ron.sprenkels@gmail.com>
2018-05-06 11:14:51 +0000

    day 012 version without datafiles

--- day_012/test_aaah.py
+++ day_012/test_aaah.py
@@ -2,16 +2,19 @@ from aaah import Aaah
 from unittest import mock
 from io import StringIO
 
-def test_1():
-    doOneCase_withfiles('testdata/aaah.1.in', 'testdata/aaah.1.ans')
-
-def test_2():
-    doOneCase_withfiles('testdata/aaah.2.in', 'testdata/aaah.2.ans')
-
 @mock.patch('sys.stdout', new_callable=StringIO)
-def doOneCase_withfiles(inFile, outFile, mock_stdout):
-    user_input = open(inFile, 'r').read().split('\n')[:-1]
-    expected_output = open(outFile, 'r').read().split('\n')[:-1]
+def test_1(mock_stdout):
+    user_input      = ['ah', 'aaah'] 
+    expected_output = ['no']
     with mock.patch('builtins.input', side_effect=user_input):
         Aaah.run()
         assert mock_stdout.getvalue() == '\n'.join(expected_output) + '\n'
+
+@mock.patch('sys.stdout', new_callable=StringIO)
+def test_2(mock_stdout):
+    user_input      = ['aah', 'ah'] 
+    expected_output = ['go']
+    with mock.patch('builtins.input', side_effect=user_input):
+        Aaah.run()
+        assert mock_stdout.getvalue() == '\n'.join(expected_output) + '\n'
+ 
