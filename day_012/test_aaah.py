abdf151908917874f041ac768134978dcaaebbe5
Ron Sprenkels <ron.sprenkels@gmail.com>
2018-05-06 13:35:29 +0000

    day 012 plain version

--- day_012/test_aaah.py
+++ day_012/test_aaah.py
@@ -1,6 +1,15 @@
-from aaah import Aaah
 from unittest import mock
 from io import StringIO
+
+class Aaah:
+
+    def run():
+        firstline = input()
+        secondline = input()
+        if (len(firstline) >= len(secondline)):
+            print('go')
+        else:
+            print('no')
 
 @mock.patch('sys.stdout', new_callable=StringIO)
 def test_1(mock_stdout):
@@ -17,4 +26,7 @@ def test_2(mock_stdout):
     with mock.patch('builtins.input', side_effect=user_input):
         Aaah.run()
         assert mock_stdout.getvalue() == '\n'.join(expected_output) + '\n'
- 
+
+if __name__ == '__main__':
+    Aaah.run()
+
