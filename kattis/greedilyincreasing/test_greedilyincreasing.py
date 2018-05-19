66051688c4aca15a97f9980e40b7841d8f41d636
Ron Sprenkels <ron.sprenkels@gmail.com>
2018-05-19 13:15:50 +0200

    fixed wrong import

--- kattis/greedilyincreasing/test_greedilyincreasing.py
+++ kattis/greedilyincreasing/test_greedilyincreasing.py
@@ -1,4 +1,4 @@
-from ceiling import Ostgotska
+from greedilyincreasing import Greedilyincreasing
 from unittest import mock
 from io import StringIO
 
@@ -13,6 +13,6 @@ def doOneCase_withfiles(inFile, outFile, mock_stdout):
     user_input = open(inFile, 'r').read().split('\n')[:-1]
     expected_output = open(outFile, 'r').read().split('\n')[:-1]
     with mock.patch('builtins.input', side_effect=user_input):
-        obj = Ostgotska()
+        obj = Greedilyincreasing()
         obj.run()
         assert mock_stdout.getvalue() == '\n'.join(expected_output) + '\n'
